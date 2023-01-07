cf2e72fd256deb782711aa9567d0772efdc1985b
LoadsX <122114788+LoadsX@users.noreply.github.com>
2023-01-07 16:32:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='LoadsDB',
     version='1.0.0',
-    packages=['LoadsDB'],
+    packages=[''],
     include_package_data=True,
     install_requires=[
         'json',
